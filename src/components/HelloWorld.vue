7e44dde910ded2f45516a31a76bd636332059ddf
santhosh kumar bollena <35809276+santhoshkumarbollena@users.noreply.github.com>
2020-10-04 10:25:41 -0500

    Done with search app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,70 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <input type="text" id="search" placeholder="Enter Id to search">
+   <button  @click="searchBasedOnId()">Search</button>
+  
+  <table>
+  <tr>
+  <td>dignosisCode</td>
+  <td>procedureCode</td>
+  <td>id</td>
+  </tr>
+  <tr v-for="search in searchResponse" :key="search.id">
+ 
+   <td>
+    {{search.dignosisCode}}
+    </td>
+    <td>{{search.procedureCode}}
+    </td><td>{{search.id}}</td>
+   
+   </tr>
+   </table>
+   
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  searchId:"",
+  searchResponse:[
+    {
+      "dignosisCode":9876,
+      "procedureCode":1234,
+      "id":1
+    },
+    {
+      "dignosisCode":9876,
+      "procedureCode":1355,
+      "id":7
+    },
+    {
+      "dignosisCode":9876,
+      "procedureCode":1123,
+      "id":8
+    }
+  ],
+    }},
+    methods:{
+        searchBasedOnId(){
+          this.searchId= document.getElementById("search").value;
+          console.log("In search"+this.searchId)
+          this.$axios
+      .post("Base Url is setted in main.js , added Url",{
+        //body
+      })
+      .then(response => {
+        console.log("Response Data",response.data)
+        
+      })
+      .catch(error => {
+        console.log("Error ",error);
+      });
+        }
+    }
+ 
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from "axios";
 
 Vue.config.productionTip = false
+
+// Integrating axios with vue as $http
+Vue.prototype.$axios = axios.create({
+  // baseURL: process.env.API_BASE_URL,
+  baseURL: "Base Url",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
 
 new Vue({
   render: h => h(App),
