f07fe5fd4af2ad5c0040ed03259bfd13c79ba762
Santhosh Kumar Bollena <s537358@nwmissouri.edu>
2020-10-07 16:21:57 -0500

    Added the bootstrap vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,67 +1,70 @@
 <template>
-  <div>
-    <input type="text" id="search" placeholder="Enter Id to search">
-   <button  @click="searchBasedOnId()">Search</button>
-  
-  <table>
-  <tr>
-  <td>dignosisCode</td>
-  <td>procedureCode</td>
-  <td>id</td>
-  </tr>
-  <tr v-for="search in searchResponse" :key="search.id">
- 
-   <td>
-    {{search.dignosisCode}}
-    </td>
-    <td>{{search.procedureCode}}
-    </td><td>{{search.id}}</td>
-   
-   </tr>
-   </table>
+<div>
+<b-container fluid>
+  <b-row class="my-1">
+    <b-col sm="4">
+     <b-form-input
+          id="search"
+          v-model="searchId"
+          type="text"
+          placeholder="Enter Id to search"
+          class="mb-2 mr-sm-2 mb-sm-0"
+        ></b-form-input>
+    </b-col>
+    <b-col sm="8" align="left" >
+      <b-button  variant="primary" @click="searchBasedOnId()">Search</b-button>
+    </b-col>
+  </b-row>
+</b-container>
+<div>
+    <b-table striped hover :items="searchResponse" class="sm-8" caption-top >
+      <template v-slot:table-caption>Below we display the search results </template>
+
+    </b-table>
+  </div>
+
    
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
+
   data() {
     return {
   name: 'HelloWorld',
   searchId:"",
-  searchResponse:[
-    {
-      "dignosisCode":9876,
-      "procedureCode":1234,
-      "id":1
-    },
-    {
-      "dignosisCode":9876,
-      "procedureCode":1355,
-      "id":7
-    },
-    {
-      "dignosisCode":9876,
-      "procedureCode":1123,
-      "id":8
-    }
-  ],
+  searchResponse:["demo","demo2","dmeo3"],
     }},
     methods:{
-        searchBasedOnId(){
-          this.searchId= document.getElementById("search").value;
-          console.log("In search"+this.searchId)
-          this.$axios
-      .post("Base Url is setted in main.js , added Url",{
-        //body
-      })
-      .then(response => {
-        console.log("Response Data",response.data)
-        
-      })
-      .catch(error => {
-        console.log("Error ",error);
-      });
+       async searchBasedOnId(){
+         
+          console.log("In search "+this.searchId)
+          try {
+                var result = await  axios({
+                    method: "POST",
+                    url: "http://localhost:8080/v1/graphql",
+                    data: {
+                        query: `
+query MyQuery {
+  hacktable2(limit: 500, where: {_or: [{dx_code: {_ilike: "%`+this.searchId+`%"}}, {dx_code_description: {_ilike: "%`+this.searchId+`%"}}]}, order_by: {rank: asc}) {
+    rank
+    dx_code
+    dx_code_description
+  }
+}
+                        `
+                    }
+                });
+                console.log(result.data.data.hacktable2)
+                this.searchResponse = result.data.data.hacktable2
+            } catch (error) {
+                console.error(error);
+            }
+
+      //Method end
         }
     }
  
